2395a0ed7b2e6a5fe60b7f1b50a8ee241c5a3753
Novium <nova@novium.space>
2024-02-05 19:12:56 -0700

    Side Menu Structure
    Started working on the classes for the side menu

---
+++ src/main/java/space/novium/client/gui/widget/VerticalExpandMenu.java
@@ -0,0 +1,8 @@
+package space.novium.client.gui.widget;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class VerticalExpandMenu {
+}
